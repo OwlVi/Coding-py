ad2dff90b4da158ecba243e24c219d15e79ff391
OwlVi <110912364+OwlVi@users.noreply.github.com>
2023-01-05 15:09:44 +0700

    update

--- CodoC++/Lab04 Function/Lab05.c
+++ CodoC++/Lab04 Function/Lab05.c
@@ -6,8 +6,7 @@ int Rangoli(int x){
     /*>>>> loop top <<<<*/
     for (int i = row/2,j =0; i > 0; i--,j = j+2)
     {       /*>>>> loop row <<<<*/
-        for (int k = column/2,l = 0  ;k >= 0 ; k--,l++)
-        {   /*>>>> loop column <<<<*/
+        for (int k = column/2,l = 0  ;k >= 0 ; k--,l++){   /*>>>> loop column <<<<*/
             if ( k == 0){
                 printf("%c",'a'+i);
             }else if (j >= k && k % 2 == 0 ){
@@ -16,8 +15,7 @@ int Rangoli(int x){
                 printf("-");
             }
         }
-        for (int k = 1 ,l = 0  ;k <= column/2 ; k++,l++)
-        {   /*>>>> loop column <<<<*/
+        for (int k = 1 ,l = 0  ;k <= column/2 ; k++,l++){   /*>>>> loop column <<<<*/
             if (j >= k && k % 2 == 0 ){
                 printf("%c",'a'+(k/2)+i);
             }else{
@@ -27,10 +25,8 @@ int Rangoli(int x){
         printf("\n");
     }
     /*>>>> loop bottom <<<<*/
-    for (int i = 0,j = row-1; i <= row/2; i++,j = j-2)
-    {       /*>>>> loop row <<<<*/
-        for (int k = column/2 ; k >= 0 ; k--)
-        {   /*>>>> loop column <<<<*/
+    for (int i = 0,j = row-1; i <= row/2; i++,j = j-2) {       /*>>>> loop row <<<<*/
+        for (int k = column/2 ; k >= 0 ; k--){   /*>>>> loop column <<<<*/
             if (j >= k && k % 2 == 0 && k != 0){
                 printf("%c",'a'+(k/2)+i);
             }else if (k == 0){
@@ -39,8 +35,7 @@ int Rangoli(int x){
                 printf("-");
             }
         }
-        for (int k = 1 ; k <= column/2 ; k++)
-        {   /*>>>> column <<<<*/
+        for (int k = 1 ; k <= column/2 ; k++){   /*>>>> column <<<<*/
             if (j >= k && k % 2 == 0 && k != 0 ){
                 printf("%c",'a'+(k/2)+i);
             }else{
--- CodoC++/TESTcode/test.c
+++ CodoC++/TESTcode/test.c
@@ -1,16 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-    char input_n[12], input_x[2];
-    fgets(input_n, 12, stdin);
-    fgets(input_x, 2, stdin);
-    int n;
-    int x;
+int Rangoli(int x){
+    int row = (x*2)-1; 
+    int column = (row*2)-1;
+    /*>>>> loop top <<<<*/
+    for (int i = row/2,j =0; i > 0; i--,j = j+2)
+    {       /*>>>> loop row <<<<*/
+        for (int k = column/2,l = 0  ;k >= 0 ; k--,l++)
+        {   /*>>>> loop column <<<<*/
+            if (j >= k && k % 2 == 0 && k != 0){
+                printf("%c",'a'+(k/2)+i);
+            }else if (k == 0){
+                printf("%c",'a'+i);
+            }else{
+                printf("-");
+            }
+        }
+        for (int k = 1 ,l = 0  ;k <= column/2 ; k++,l++)
+        {   /*>>>> loop column <<<<*/
+            if (j >= k && k % 2 == 0 && k != 0 ){
+                printf("%c",'a'+(k/2)+i);
+            }else{
+                printf("-");
+            }
+        }
+        printf("\n");
+    }
+    /*>>>> loop bottom <<<<*/
+    for (int i = 0,j = row-1; i <= row/2; i++,j = j-2)
+    {       /*>>>> loop row <<<<*/
+        for (int k = column/2 ; k >= 0 ; k--)
+        {   /*>>>> loop column <<<<*/
+            if (j >= k && k % 2 == 0 && k != 0){
+                printf("%c",'a'+(k/2)+i);
+            }else if (k == 0){
+                printf("%c",'a'+i);
+            }else{
+                printf("-");
+            }
+        }
+        for (int k = 1 ; k <= column/2 ; k++)
+        {   /*>>>> column <<<<*/
+            if (j >= k && k % 2 == 0 && k != 0 ){
+                printf("%c",'a'+(k/2)+i);
+            }else{
+                printf("-");
+            }
+        }
+        printf("\n");
+    }
+}
 
-    n = atoi(input_n);
-    x = atoi(input_x);
-
-    printf("%d",n%10);
-
+int main(){
+    char input_n[5];
+    fgets(input_n,5,stdin);
+    int n = atoi(input_n);
+    if (n <= 26 && n > 0){
+        Rangoli(n);
+    }else{
+        printf("-");
+    }
 }
